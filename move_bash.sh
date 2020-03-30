fb0707035f05898091f453d233569105c89abc13
Doomd <bcosteloe@gmail.com>
2020-03-29 21:15:01 -0600

    Innitial Commit

---
+++ move_bash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Edit these paths if you need/want to:
+destPath="archive" # destination parent path/folder where we'll copy our path structure
+pList="path_list.txt" # text file list of all paths we should include in our filename search
+fList="file_list.txt" # text file list of all filenames to move
+
+# DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING
+
+pathList=()
+while IFS= read -r line || [[ "$line" ]];
+    do
+        pathList+=("$line")
+    done < $pList
+
+for p in "${pathList[@]}"
+    do
+        mkdir -p $destPath/$p
+    done
+
+while IFS= read -r file;
+    do
+        for p in "${pathList[@]}"
+            do
+                mv $p/$file $destPath/$p/$file
+            done
+    done < $fList
---
+++ source_files/LICENSE.md
@@ -0,0 +1,4 @@
+### PUBLIC DOMAIN IMAGES
+All sample images included in this package are in the public domain and were downloaded from this website:
+
+https://www.pexels.com/public-domain-images/
