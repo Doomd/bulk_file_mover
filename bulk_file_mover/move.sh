22844aca6e8e04f088d9944149312de16f3c4a31
Doomd <bcosteloe@gmail.com>
2020-06-06 20:53:24 -0600

    cleaned up file formatting and log output to be cleaner and timestamps more accurate

--- bulk_file_mover/move.sh
+++ bulk_file_mover/move.sh
@@ -5,17 +5,16 @@ pathListFile="path_list.txt" # text file list of all top level directory paths w
 fileListFile="file_list.txt" # text file list of all filenames to move
 subDirDepth=5 # how many potential subdirectories should we include/search?
 includeParentDir="yes" # "yes/no" include files IN the root/parent directory?
-
 #### COPY, MOVE, or DELETE! ####
 action="cp -p"
 # choose "cp -p" (for copy with optional timestamp preservation)
 # or "mv" for move
 # or "rm" for delete
 
-#### DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING
 
-# relevantFiles=(imagefolder/*.jpg); echo $#relevantFiles
-parentDir=".." # Root/Parent Directory to begin file search
+#### DO NOT EDIT BELOW THIS LINE UNLESS YOU KNOW WHAT YOU'RE DOING
+scriptDir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+parentDir="$(dirname "$scriptDir")"
 parentPath="$parentDir/"
 if [[ $action == *"mv"* ]]; then
 	actionWord="moved"
@@ -26,26 +25,27 @@ fi
 if [[ $action == *"rm"* ]]; then
 	actionWord="removeD"
 fi
-ts_title="$(date +%Y-%m-%d-%H-%M-%S)"
-ts_indented="  $(date +%Y-%m-%d-%H-%M-%S)"
-destPath="backups"
+ts_title() {
+	date +"%Y-%m-%d-%H-%M-%S"
+}
+ts_indented() {
+	date +"  %Y-%m-%d-%H-%M-%S"
+}
+destPath="$scriptDir/backups"
 destFilePath="$destPath/files"
-
-# Log Location:
-logLocation="$destPath/logs/$ts_title"
+# Log Locat1ion:
+logLocation="$destPath/logs/$(ts_title)"
 log_all="$logLocation/all.log"
 log_files_touched="$logLocation/files_touched.log"
+log_files_movement="$logLocation/files_movement.log"
 log_files_not_touched="$logLocation/files_not_touched.log"
 mkdir -p "$logLocation"
-
 title="Doomd's Bulk File Mover"
 author="https://github.com/Doomd"
 echo -en "$title ~ $author" >> $log_all
-echo -en "\033[43m\033[30m$title\033[m\033[m \033[33m~ $author\033[m"
-
+echo -en "\n\033[43m\033[30m$title\033[m\033[m \033[33m~ $author\033[m"
 echo -en "\n\nCopied/Moved files will be placed in:\n  '\033[32m$destFilePath\033[m'"
 echo -en "\nLog files for this session will be created in:\n  '\033[32m$logLocation\033[m'"
-
 pathList=() ## Array for our list of directories to search
 allPaths=() ## Array for ALL directories to search, including subdirectories we find
 if [ "$includeParentDir" = "yes" ]; then
@@ -66,127 +66,147 @@ while IFS= read -r file || [ -n "$file" ];
         base=${file%.*}
 		extension=${file#$base.}
 		if [[ "$extension"  =~ "${fileExtensions[@]}" ]]; then
-			echo "  DUPLICATE file extention, '$extension' (from '$file'). Skipped."
+			echo "$(ts_indented) > DUPLICATE file extention, '$extension' (from '$file'). Skipped." | tee -a $log_all
 		else
 			if [[ ! "$extension"  =~ "${fileExtensions[@]}" ]]; then
 				fileExtensions+=("$extension")
-				echo "  UNIQUE file extention, '$extension' (from '$file') added to fileExtensions array."
+				echo "$(ts_indented) > UNIQUE file extention, '$extension' (from '$file') added to fileExtensions array." | tee -a $log_all
 			fi
 		fi
    	done < $fileListFile
 
-trim() {
-    local var="$*"
-    # remove leading whitespace characters
-    var="${var#"${var%%[![:space:]]*}"}"
-    # remove trailing whitespace characters
-    var="${var%"${var##*[![:space:]]}"}"
-    printf '%s' "$var"
-}
-
 i=1 ## create an index for our allPaths array (compatible w bash & zsh)
-
 ## for every directory in path list...
 pathsToSearch="PATHS to Search:"
 echo -en "\n$pathsToSearch\n" >> $log_all
 echo -en "\n\033[42m\033[30m$pathsToSearch\033[m\n"
 for p in "${pathList[@]}"
 	do
-		if [[ " ${allPaths[@]} " =~ " $p " ]]; then
-			## if a subdirectory already matches a listed path, skip
-			echo "$ts_indented > A listed path [$p] is a duplicate of an allPaths array item. Skipping it." | tee -a $log_all
-		fi
-		if [[ ! " ${allPaths[@]} " =~ " $p " ]] ; then
-			## if the path is the parent directory...
-			if [[ "$p" =~ "$parentDir" ]]; then
-				allPaths[$i]=$p
-				echo "$ts_indented > allPaths[$i] = '${allPaths[i]}' (Parent Directory)" | tee -a $log_all
-				((i++))
+			if [[ " ${allPaths[@]} " =~ " $p " ]]; then
+				## if a subdirectory already matches a listed path, skip
+				echo "$(ts_indented) > A listed path [$p] is a duplicate of an allPaths array item. Skipping it." | tee -a $log_all
 			fi
-			## if the path is not the parent directory, add the parent directory to path
-			if [[ ! "$p" =~ "$parentDir" ]]; then
-				allPaths[$i]="$parentDir/$p"
-				echo "$ts_indented > allPaths[$i] = '${allPaths[i]}' (Listed Subdirectory)" | tee -a $log_all
-				((i++))
-				## ...and for each subdirectory of all paths...
-				for subdir in $(find $parentDir/$p -maxdepth $subDirDepth -type d)
-					do
-						if [ -d "$subdir" ]; then
-							if [[ " ${allPaths[@]} " =~ " ${subdir} " ]]; then
-								echo "$ts_indented > A found subdirectory [${subdir}] is a duplicate of a allPaths path. Skipping it." | tee -a $log_all
-							else
-								for fileType in "${fileExtensions[@]}"
-									do
-										numFiles=$((`ls ${subdir} | grep $fileType | wc -l`))
-										if [ $numFiles -gt 0 ]; then
-											echo "  There are [$numFiles] files with '$fileType' ext in '$parentDir/${subdir}'" | tee -a $log_all
-											if [[ ! " ${allPaths[@]} " =~ " $parentDir/${subdir} " ]]; then
-												allPaths[$i]="$subdir"
-												echo "$ts_indented > allPaths[$i] = '${allPaths[i]}' (Found Subdirectory)" | tee -a $log_all
-												((i++))
-												break
-											fi
-										else
-											echo "  There are 0 files with '$fileType' ext in '$parentDir/${subdir}'. Skipping it." | tee -a $log_all
-										fi
-
-									done
+			if [[ ! " ${allPaths[@]} " =~ " $p " ]] ; then
+				## if the path is the parent directory...
+				if [[ "$p" =~ "$parentDir" ]]; then
+					## only add parent path to allPaths array if it actually has the filetypes in our list
+					for fileType in "${fileExtensions[@]}"
+						do
+							numFiles=$((`ls $p | grep $fileType | wc -l`))
+							if [ $numFiles -gt 0 ]; then
+								echo "$(ts_indented) > There are $numFiles files with '$fileType' ext in '$p'" | tee -a $log_all
+								allPaths[$i]=$p
+								echo "$(ts_indented) > allPaths[$i] = '${allPaths[i]}' (Parent Directory)" | tee -a $log_all
+								((i++))
+								break
 							fi
-						fi
-					done
+						done
+				fi
+				## if the path is not the parent directory, add the parent directory to path
+				if [[ ! "$p" =~ "$parentDir" ]]; then
+					## only add path to allPaths array if it actually has the filetypes in our list
+					for fileType in "${fileExtensions[@]}"
+							do
+								numFiles=$((`ls $parentDir/$p | grep $fileType | wc -l`))
+								if [ $numFiles -gt 0 ]; then
+									echo "$(ts_indented) > There are $numFiles files with '$fileType' ext in '$p'" | tee -a $log_all
+									allPaths[$i]="$parentDir/$p"
+									echo "$(ts_indented) > allPaths[$i] = '${allPaths[i]}' (Listed Directory)" | tee -a $log_all
+									((i++))
+									break
+								fi
+							done
+					## ...and for each subdirectory of all paths...
+					for subdir in $(find $parentDir/$p -maxdepth $subDirDepth -type d)
+						do
+							if [ -d "$subdir" ]; then
+								if [[ " ${allPaths[@]} " =~ " ${subdir} " ]]; then
+									echo "$(ts_indented) > '${subdir}' subdirectory was found, but is a duplicate of an allPaths path. Skipping it." | tee -a $log_all
+								else
+									## only add subdirectory to allPaths array if it actually has the filetypes in our list
+									for fileType in "${fileExtensions[@]}"
+										do
+											numFiles=$((`ls ${subdir} | grep $fileType | wc -l`))
+											if [ $numFiles -gt 0 ]; then
+												echo "$(ts_indented) > There are $numFiles files with '$fileType' ext in '${subdir}'" | tee -a $log_all
+												if [[ ! " ${allPaths[@]} " =~ " $parentDir/${subdir} " ]]; then
+													allPaths[$i]="$subdir"
+													echo "$(ts_indented) > allPaths[$i] = '${allPaths[i]}' (Found Subdirectory)" | tee -a $log_all
+													((i++))
+													break
+												fi
+											else
+												echo "$(ts_indented) > There are 0 files with '$fileType' ext in '${subdir}'. Skipping it." | tee -a $log_all
+											fi
+										done
+								fi
+							fi
+						done
+				fi
 			fi
-		fi
 	done
 
 filesAction="FILES Action:"
 echo -en "\n$filesAction\n" >> $log_all
 echo -en "\n\033[42m\033[30m$filesAction\033[m\n"
-
-files_touched_header="Files $actionWord [ $ts_title ]\n\n"
+files_touched_header="Files $actionWord [ $(ts_title) ]\n\n"
 echo -en $files_touched_header >> $log_files_touched
-files_not_touched_header="Files NOT Found (and NOT $actionWord) [ $ts_title ]\n\n"
+files_movement_header="Files $actionWord from source to destination [ $(ts_title) ]\n\n"
+echo -en $files_movement_header >> $log_files_movement
+files_not_touched_header="Files NOT Found (and NOT $actionWord) [ $(ts_title) ]\n\n"
 echo -en $files_not_touched_header >> $log_files_not_touched
-
 while IFS= read -r file || [ -n "$file" ];
-    do
-        for path in "${allPaths[@]}"
+	do
+		for path in "${allPaths[@]}"
 			do
 				if test -f "$path/$file"; then
-					echo "$ts_indented > '$file' was found in '$path' directory." | tee -a $log_all
+					#echo -en "\n" | tee -a $log_all
+					echo "$(ts_indented) > '$file' was found in '$path' directory." | tee -a $log_all
 					if [ "$path" = "$parentDir" ]; then
 						if [ -d " $destFilePath " ]; then
-							echo "$ts_indented > '$destFilePath' directory already exists." | tee -a $log_all
+							echo "$(ts_indented) > '$destFilePath' directory already exists." | tee -a $log_all
 						else
 							mkdir -p "$destFilePath"
-							echo "$ts_indented > '$destFilePath' directory created." | tee -a $log_all
+							echo "$(ts_indented) > '$destFilePath' directory created." | tee -a $log_all
 						fi
 					else
 						noParentPath="${path#$parentPath}"
 						if [ -d " $destFilePath/$path " ]; then
-							echo "$ts_indented > '$destFilePath/$path' directory already exists." | tee -a $log_all
+							echo "$(ts_indented) > '$destFilePath/$path' directory already exists." | tee -a $log_all
 						else
 							mkdir -p "$destFilePath/$noParentPath"
-							echo "$ts_indented > '$destFilePath/$noParentPath' directory created." | tee -a $log_all
+							echo "$(ts_indented) > '$destFilePath/$noParentPath' directory created." | tee -a $log_all
 						fi
 					fi
 					## if the file is IN the root/parent directory...
 					if [ "$path" = "$parentDir" ]; then
 						$action "$path/$file" "$destFilePath/$file"
-						echo "$ts_indented > '$file' $actionWord from '$path' to '$destFilePath'" | tee -a $log_all
-						echo "$destFilePath/$file" >> $log_files_touched
+						echo -en "$(ts_indented) > '$file' $actionWord to '$destFilePath'" | tee -a $log_all
+						echo "'$destFilePath/$file'" >> $log_files_touched
+						echo "'$path/$file' was $actionWord to '$destFilePath/$file'" >> $log_files_movement
 					## else if it's in a subfolder(s) of the parent directory
 					else
 						$action "$path/$file" "$destFilePath/$noParentPath/$file"
-						echo "$ts_indented > '$file' $actionWord from '$path' to '$destFilePath/$noParentPath'" | tee -a $log_all
-						echo "$destFilePath/$noParentPath/$file" >> $log_files_touched
+						echo -en "$(ts_indented) > '$file' $actionWord to '$destFilePath/$noParentPath'" | tee -a $log_all
+						echo "'$destFilePath/$noParentPath/$file'" >> $log_files_touched
+						echo "'$path/$file' was $actionWord to '$destFilePath/$noParentPath/$file'" >> $log_files_movement
 					fi
+					echo -en "\n" | tee -a $log_all
+					reject=""
 				else
-					echo "$ts_indented > '$file' was not found in '$path' directory" | tee -a $log_all
-					if [ "$path" = "$parentDir" ]; then
-						echo "$destFilePath/$file" >> $log_files_not_touched
-					else
-						echo "$destFilePath/$noParentPath/$file" >> $log_files_not_touched
-					fi
+					echo "$(ts_indented) > '$file' was not found in '$path'" | tee -a $log_all
+					reject="$file"
 				fi
 			done
-   done < $fileListFile
+			if [ -z "$reject" ]; then
+				echo -en "\n" | tee -a $log_all
+			else
+				# echo "reject file is $reject"
+				echo -en "\n" | tee -a $log_all
+				echo "'$file'" >> $log_files_not_touched
+			fi
+	done < $fileListFile
+
+echo -en "In case you missed it...Copied/Moved files are in:\n  '\033[32m$destFilePath\033[m'"
+echo -en "\nLog files for this session are in:\n  '\033[32m$logLocation\033[m'"
+echo -en "\n\n\033[33mAll Done!\nNeed help? $author\033[m\n\n"
